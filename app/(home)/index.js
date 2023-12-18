b208f620da5031c1a2902e8921391ed2e2aea21a
RadamantiumX <eduardo2502@gmail.com>
2023-12-17 21:31:54 -0300

    project init

---
+++ app/(home)/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <ScrollView>
+        <View>
+            <Text>
+                Conn Urban
+            </Text>
+        </View>
+    </ScrollView>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
